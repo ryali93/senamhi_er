f589ddfa49d00cf211d1814a843e39275b631958
Roy Yali <ryali93@gmail.com>
2021-02-22 00:01:54 -0500

    funcional pero falta responsive

--- docs/assets/js/map-leaflet.js
+++ docs/assets/js/map-leaflet.js
@@ -1,4 +1,4 @@
-var basemap1 = function() {
+var bm_stamen = function() {
     var attr = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.';
     return L.tileLayer("http://tile.stamen.com/toner-background/{z}/{x}/{y}.png", {
         opacity: 0.3,
@@ -6,9 +6,24 @@ var basemap1 = function() {
     });
 }
 
-var basemap2 = function() {
-    var attr = 'openlayers';
+var bm_openstreetmap = function() {
+    var attr = 'OpenStreetMap';
     return L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution: attr
+    });
+}
+
+var bm_opentopomap = function(){
+    var attr = 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)';
+    return L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+        maxZoom: 17,
+        attribution: attr
+    });
+}
+    
+var bm_satellite = function(){
+    var attr = 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community';
+    return L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
         attribution: attr
     });
 }
@@ -26,10 +41,12 @@ var create_point = function(){
     });
 }
 
+var changeVisibility = function() {
+    document.getElementById("exportar_tabla").style.visibility = "visible";
+}
+
 var generar_tb_pp = function(trs_tr) {
     var data_regiones = JSON.parse(datos_regiones)[0];
-    console.log(data_regiones);
-    console.log(trs_tr);
 
     var tr_anos = ["TR2","TR5","TR10","TR30","TR50","TR75","TR100","TR200","TR500","TR1000"];
     var region = document.getElementById("infoRegion").innerText.replace("Subregión: ","");
@@ -42,8 +59,10 @@ var generar_tb_pp = function(trs_tr) {
     var tabla_datos = "<table id='dtHorizontal' class='table table-striped' width=100%>";
     tabla_datos += "<thead><tr><th align=center>Duración</th>";
 
+    var datos_hietograma = {};
     for(cab=0;cab<=9;cab++){
         tabla_datos+="<th>"+tr_anos[cab]+"</th>";
+        datos_hietograma[tr_anos[cab]] = [];
     };
 
     tabla_datos += "</tr></thead><tbody>";
@@ -57,15 +76,15 @@ var generar_tb_pp = function(trs_tr) {
         tabla_datos += "<tr><td align=center>"+n+"-hr";
 
         for(m=0;m<=9;m++){
-            var val_hiet_mn = (val_pp_mn[j]*trs_tr["LI_"+tr_anos[m]]).toFixed(2);
-            var val_hiet_me = (val_pp_me[j]*trs_tr["LM_"+tr_anos[m]]).toFixed(2);
-            var val_hiet_mx = (val_pp_mx[j]*trs_tr["LS_"+tr_anos[m]]).toFixed(2);
-
-            console.log(val_hiet_me);
+            var val_hiet_mn = (val_pp_mn[j]*trs_tr["LI_"+tr_anos[m]]).toFixed(1);
+            var val_hiet_me = (val_pp_me[j]*trs_tr["LM_"+tr_anos[m]]).toFixed(1);
+            var val_hiet_mx = (val_pp_mx[j]*trs_tr["LS_"+tr_anos[m]]).toFixed(1);
 
             val_temp_mn = val_hiet_mn;
             val_temp_me = val_hiet_me;
             val_temp_mx = val_hiet_mx;
+
+            datos_hietograma[tr_anos[m]].push(parseFloat(val_temp_me));
             // if(n==1){
             //     val_temp_mn = val_hiet_mn;
             //     val_temp_me = val_hiet_me;
@@ -75,40 +94,115 @@ var generar_tb_pp = function(trs_tr) {
             //     val_temp_me = val_pp_me[j]*trs_tr["LM_"+tr_anos[m]] - val_pp_me[j-1]*trs_tr["LM_"+tr_anos[m]];
             //     val_temp_mx = val_pp_mx[j]*trs_tr["LS_"+tr_anos[m]] - val_pp_mx[j-1]*trs_tr["LS_"+tr_anos[m]];
             // };
-            console.log(val_temp_me);
-
             tabla_datos += "</td><td align=center style='padding: 0px;'>";
-            tabla_datos += "<strong>"+parseFloat(val_temp_me).toFixed(2)+"</strong>";
-            tabla_datos += "("+parseFloat(val_temp_mn).toFixed(2)+"-"+parseFloat(val_temp_mx).toFixed(2)+")";
+            tabla_datos += "<strong>"+parseFloat(val_temp_me).toFixed(1)+"</strong>";
+            tabla_datos += "("+parseFloat(val_temp_mn).toFixed(1)+"-"+parseFloat(val_temp_mx).toFixed(1)+")";
             tabla_datos += "</td>";
         }
         tabla_datos += "</tr>"
-
-        // var val_hiet_mn = (val_pp_mn[j]*trs_tr).toFixed(2);
-        // var val_hiet_me = (val_pp_me[j]*trs_tr).toFixed(2);
-        // var val_hiet_mx = (val_pp_mx[j]*trs_tr).toFixed(2);
-
-        // if(n==1){
-        //     val_temp_mn = val_hiet_mn;
-        //     val_temp_me = val_hiet_me;
-        //     val_temp_mx = val_hiet_mx;
-        // }else{
-        //     val_temp_mn = val_pp_mn[j]*trs_tr - val_pp_mn[j-1]*trs_tr;
-        //     val_temp_me = val_pp_me[j]*trs_tr - val_pp_me[j-1]*trs_tr;
-        //     val_temp_mx = val_pp_mx[j]*trs_tr - val_pp_mx[j-1]*trs_tr;
-        // };
-        // console.log(val_temp_me);
-        // tabla_datos += "</td><td align=center>"+val_pp_me[j];
-        // tabla_datos += "</td><td align=center>"+val_hiet_me;
-        // tabla_datos += "</td><td align=center style='padding: 0px;'>";
-        // tabla_datos += "<strong>"+parseFloat(val_temp_me).toFixed(2)+"</strong>";
-        // tabla_datos += "("+parseFloat(val_temp_mn).toFixed(2)+"-"+parseFloat(val_temp_mx).toFixed(2)+")";
-        // tabla_datos += "</td></tr>";
-        // val_temp = parseFloat(val_hiet) - val_temp
     }
     tabla_datos += "</tbody></table>";
-    document.getElementById("tabla_datos").innerHTML = tabla_datos;
 
+    tabla_datos += '<br><br><button onclick="exportTableToExcel(';
+    tabla_datos += "'tabla_datos'";
+    tabla_datos += ')>Exportar a Excel</button>';
+    console.log(datos_hietograma);
+    document.getElementById("tabla_datos").innerHTML = tabla_datos;
+    create_graph(datos_hietograma)
+
+}
+
+var create_graph2 = function(datos){}
+
+var create_graph = function(datos){
+    var graph_ddf = document.getElementById("graph_ddf");
+    var graph_TR2 = {
+        label: "TR2",
+        data: datos["TR2"],
+        borderColor: 'red',
+        fill: false,
+        pointStyle: 'line'
+    };
+
+    var graph_TR5 = {
+        label: "TR5",
+        data: datos["TR5"],
+        borderColor: 'blue',
+        fill: false,
+        pointStyle: 'line'
+    };
+
+    var graph_TR10 = {
+        label: "TR10",
+        data: datos["TR10"],
+        borderColor: 'pink',
+        fill: false,
+        pointStyle: 'line'
+    };
+
+    var graph_TR30 = {
+        label: "TR30",
+        data: datos["TR30"],
+        borderColor: 'black',
+        fill: false,
+        pointStyle: 'line'
+    };
+
+    var graph_TR50 = {
+        label: "TR50",
+        data: datos["TR50"],
+        borderColor: '#98B9AB',
+        fill: false,
+        pointStyle: 'line'
+    };
+
+    var graph_TR100 = {
+        label: "TR100",
+        data: datos["TR100"],
+        borderColor: '#F9B90AFF',
+        fill: false,
+        pointStyle: 'line'
+    };
+
+    var graph_TR500 = {
+        label: "TR500",
+        data: datos["TR500"],
+        borderColor: '#34A74BFF',
+        fill: false,
+        pointStyle: 'line'
+    };
+
+    var graph_TR1000 = {
+        label: "TR1000",
+        data: datos["TR1000"],
+        borderColor: '#AFA100',
+        fill: false,
+        pointStyle: 'line'
+    };
+
+    var TR_data = {
+      labels: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24],
+      datasets: [graph_TR2, graph_TR5, graph_TR10, graph_TR30, graph_TR50, graph_TR100, graph_TR500, graph_TR1000]
+    };
+
+    var chartOptionsLine = {
+      fontSize:10,
+      legend: {
+        display: true,
+        position: 'bottom',
+        labels: {
+          fontColor: 'black',
+          fontSize: 10,
+          usePointStyle: true
+        }
+      }
+    };
+
+    var lineChart = new Chart(graph_ddf, {
+      type: 'line',
+      data: TR_data,
+      options: chartOptionsLine
+    });
 }
 
 var exportTableToExcel = function(tableID, filename = ''){
@@ -141,19 +235,32 @@ var funcion_inicial = function(){
     map.setView([-10, -82], 6);
 
     // Capas principales
-    var regiones = L.tileLayer.wms("http://localhost:8080/geoserver/senamhi_v1/wms", {
-      layers: "senamhi_v1:gpo_regiones_pp_max",
+    var cuencas = L.tileLayer.wms("http://localhost:8080/geoserver/dhi/wms", {
+      layers: "dhi:gpo_regiones_hidrograficas",
       format: 'image/png',
       transparent: true
     });
 
-    var departamentos = L.tileLayer.wms("http://localhost:8080/geoserver/bd/wms", {
-       layers: "bd:gpo_departamentos",
+    var departamentos = L.tileLayer.wms("http://localhost:8080/geoserver/dhi/wms", {
+       layers: "dhi:gpo_departamentos",
+       format: 'image/png',
+       transparent: true
+    });
+
+    var provincias = L.tileLayer.wms("http://localhost:8080/geoserver/dhi/wms", {
+       layers: "dhi:gpo_provincias",
+       format: 'image/png',
+       transparent: true
+    });
+
+    var regionespp = L.tileLayer.wms("http://localhost:8080/geoserver/dhi/wms", {
+       layers: "dhi:gpo_regiones_pp",
        format: 'image/png',
        transparent: true
     });
 
     var quartiles = L.tileLayer.wms("http://localhost:8080/geoserver/senamhi_v1/wms", {
+       opacity: 0.3,
        layers: "senamhi_v1:q_pp",
        format: 'image/png',
        transparent: true
@@ -161,24 +268,38 @@ var funcion_inicial = function(){
 
     // Mapas base
     var basemaps = {
-        'Stamen': basemap1().addTo(map),
-        'OpenStreetMap': basemap2().addTo(map),
+        'Stamen': bm_stamen().addTo(map),
+        'OpenStreetMap': bm_openstreetmap().addTo(map),
+        'OpenTopoMap': bm_opentopomap().addTo(map),
+        'Satellite': bm_satellite().addTo(map),
         'Blank': blank()
     };
 
     var layers = {
         'Quartiles': quartiles.addTo(map),
+        'Regiones pp': regionespp.addTo(map),
         'Departamentos': departamentos.addTo(map),
-        'Regiones': regiones.addTo(map),
+        'Provincias': provincias.addTo(map),
+        'Cuencas Hidrograficas': cuencas.addTo(map),
     };
 
     L.control.layers(basemaps, layers, {collapsed: false}).addTo(map);
+    L.control.betterscale().addTo(map);
 
     var sidebar = L.control.sidebar('sidebar_leaflet').addTo(map);
     sidebar.open('lluvias');
+    var markerIcon = L.icon({
+        iconUrl: 'assets/img/marker.png',
+        iconAnchor:   [0.5, 1]
+    });
+    var marker = null;
 
     var source_url = "http://localhost:8080/geoserver/senamhi_v1/wms";
     map.on('click', function (evt) {
+        if (marker !== null) {
+            map.removeLayer(marker);
+        }
+        marker = L.marker(evt.latlng, {icon: markerIcon}).addTo(map);
         document.getElementById('coord_x').innerHTML = "<b>X (wgs84): </b>" + evt.latlng["lng"].toFixed(3) + "°";
         document.getElementById('coord_y').innerHTML = "<b>Y (wgs84): </b>" + evt.latlng["lat"].toFixed(3) + "°";
         getFeatureInfo(evt, "senamhi_v1:gpo_regiones_pp_max");
@@ -225,25 +346,14 @@ var funcion_inicial = function(){
                     else{
                         console.log("Crear Tabla aqui");
                         generar_tb_pp(info_data);
+                        changeVisibility();
                     }
                 });
 
         }
 
     };
-
-    var markerIcon = L.icon({
-        iconUrl: 'assets/img/marker.png',
-        iconAnchor:   [0.5, 1]
-    });
-    var marker = null;
-    map.on('click', function (e) {
-        if (marker !== null) {
-            map.removeLayer(marker);
-        }
-        marker = L.marker(e.latlng, {icon: markerIcon}).addTo(map);
-    });
 }
 
 funcion_inicial()
-create_point()
+// create_point()
